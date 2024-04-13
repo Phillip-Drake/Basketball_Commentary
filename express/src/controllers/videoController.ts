c2716344e212131679d378ce24b9af6f55af8d75
Levi <lccrider@uark.edu>
2024-04-12 21:01:27 -0500

    Uploads are now working

--- express/src/controllers/videoController.ts
+++ express/src/controllers/videoController.ts
@@ -1,4 +1,17 @@
 import multer from "multer";
+import fs from "fs";
 
-const upload = multer({ dest: "uploads/" });
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    fs.readdir("uploads/", (err, files) => {
+      let count = files ? files.length : 0;
+      cb(null, count + 1 + ".mp4");
+    });
+  },
+});
+
+const upload = multer({ storage: storage });
 export const uploadFile = upload.single("file");
--- express/src/express-server.ts
+++ express/src/express-server.ts
@@ -5,7 +5,7 @@ const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/video", videoRouter);
+app.use("/upload", videoRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
